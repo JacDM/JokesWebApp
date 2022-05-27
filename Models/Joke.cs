ff35a92bd9e1c5d76276b54720ad6fbcbcba5a5e
JacDM <jackdanielmoseshs@gmail.com>
2022-05-27 22:23:36 +0400

    added gitignore

---
+++ Models/Joke.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+            
+        }
+
+    }
+}
